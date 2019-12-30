83d8dd58684c4c652cf569a356ad75bbe301185c
Konstantin Matsiushonok <k.matush@yandex.com>
2019-12-30 12:15:40 +0300

    Update init.el

--- init.el
+++ init.el
@@ -52,7 +52,8 @@
  '(custom-enabled-themes (quote (sanityinc-tomorrow-night)))
  '(custom-safe-themes
    (quote
-    ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "12bacee81d067acf07dec4c867be541a04744a6ac6a39636de25a2c77e9b573c" "43c808b039893c885bdeec885b4f7572141bd9392da7f0bd8d8346e02b2ec8da" "8aca557e9a17174d8f847fb02870cb2bb67f3b6e808e46c0e54a44e3e18e1020" "6b289bab28a7e511f9c54496be647dc60f5bd8f9917c9495978762b99d8c96a0" "93a0885d5f46d2aeac12bf6be1754faa7d5e28b27926b8aa812840fe7d0b7983" "75d3dde259ce79660bac8e9e237b55674b910b470f313cdf4b019230d01a982a" "10461a3c8ca61c52dfbbdedd974319b7f7fd720b091996481c8fb1dded6c6116" "151bde695af0b0e69c3846500f58d9a0ca8cb2d447da68d7fbf4154dcf818ebc" "d1b4990bd599f5e2186c3f75769a2c5334063e9e541e37514942c27975700370" "cd736a63aa586be066d5a1f0e51179239fe70e16a9f18991f6f5d99732cabb32" "6b2636879127bf6124ce541b1b2824800afc49c6ccd65439d6eb987dbf200c36" "49ec957b508c7d64708b40b0273697a84d3fee4f15dd9fc4a9588016adee3dad" "9954ed41d89d2dcf601c8e7499b6bb2778180bfcaeb7cdfc648078b8e05348c6" "6d589ac0e52375d311afaa745205abb6ccb3b21f6ba037104d71111e7e76a3fc" "fe666e5ac37c2dfcf80074e88b9252c71a22b6f5d2f566df9a7aa4f9bea55ef8" "a8c210aa94c4eae642a34aaf1c5c0552855dfca2153fa6dd23f3031ce19453d4" "f0dc4ddca147f3c7b1c7397141b888562a48d9888f1595d69572db73be99a024" "d2e9c7e31e574bf38f4b0fb927aaff20c1e5f92f72001102758005e53d77b8c9" "a3fa4abaf08cc169b61dea8f6df1bbe4123ec1d2afeb01c17e11fdc31fc66379" "fd944f09d4d0c4d4a3c82bd7b3360f17e3ada8adf29f28199d09308ba01cc092" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "274fa62b00d732d093fc3f120aca1b31a6bb484492f31081c1814a858e25c72e" default)))
+    ("2878517f049b28342d7a360fd3f4b227086c4be8f8409f32e0f234d129cee925" "2d1fe7c9007a5b76cea4395b0fc664d0c1cfd34bb4f1860300347cdad67fb2f9" "67798cfaf7b064072bf519ed1ade02a8f4412df89c560e35f25d1936cf35b8ce" "f30aded97e67a487d30f38a1ac48eddb49fdb06ac01ebeaff39439997cbdd869" "f2b83b9388b1a57f6286153130ee704243870d40ae9ec931d0a1798a5a916e76" "428754d8f3ed6449c1078ed5b4335f4949dc2ad54ed9de43c56ea9b803375c23" "d6f04b6c269500d8a38f3fabadc1caa3c8fdf46e7e63ee15605af75a09d5441e" "70ed3a0f434c63206a23012d9cdfbe6c6d4bb4685ad64154f37f3c15c10f3b90" "332e009a832c4d18d92b3a9440671873187ca5b73c2a42fbd4fc67ecf0379b8c" "0fe9f7a04e7a00ad99ecacc875c8ccb4153204e29d3e57e9669691e6ed8340ce" "fe76f3d5094967034192f6a505085db8db6deb0e135749d9a54dc488d6d3ee2f" "0d087b2853473609d9efd2e9fbeac088e89f36718c4a4c89c568dd1b628eae41" "70cc30fd9d27a8d0d3ae82974ac2c409fd2cd5746470e2246778c6bec2d4857c" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "12bacee81d067acf07dec4c867be541a04744a6ac6a39636de25a2c77e9b573c" "43c808b039893c885bdeec885b4f7572141bd9392da7f0bd8d8346e02b2ec8da" "8aca557e9a17174d8f847fb02870cb2bb67f3b6e808e46c0e54a44e3e18e1020" "6b289bab28a7e511f9c54496be647dc60f5bd8f9917c9495978762b99d8c96a0" "93a0885d5f46d2aeac12bf6be1754faa7d5e28b27926b8aa812840fe7d0b7983" "75d3dde259ce79660bac8e9e237b55674b910b470f313cdf4b019230d01a982a" "10461a3c8ca61c52dfbbdedd974319b7f7fd720b091996481c8fb1dded6c6116" "151bde695af0b0e69c3846500f58d9a0ca8cb2d447da68d7fbf4154dcf818ebc" "d1b4990bd599f5e2186c3f75769a2c5334063e9e541e37514942c27975700370" "cd736a63aa586be066d5a1f0e51179239fe70e16a9f18991f6f5d99732cabb32" "6b2636879127bf6124ce541b1b2824800afc49c6ccd65439d6eb987dbf200c36" "49ec957b508c7d64708b40b0273697a84d3fee4f15dd9fc4a9588016adee3dad" "9954ed41d89d2dcf601c8e7499b6bb2778180bfcaeb7cdfc648078b8e05348c6" "6d589ac0e52375d311afaa745205abb6ccb3b21f6ba037104d71111e7e76a3fc" "fe666e5ac37c2dfcf80074e88b9252c71a22b6f5d2f566df9a7aa4f9bea55ef8" "a8c210aa94c4eae642a34aaf1c5c0552855dfca2153fa6dd23f3031ce19453d4" "f0dc4ddca147f3c7b1c7397141b888562a48d9888f1595d69572db73be99a024" "d2e9c7e31e574bf38f4b0fb927aaff20c1e5f92f72001102758005e53d77b8c9" "a3fa4abaf08cc169b61dea8f6df1bbe4123ec1d2afeb01c17e11fdc31fc66379" "fd944f09d4d0c4d4a3c82bd7b3360f17e3ada8adf29f28199d09308ba01cc092" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "274fa62b00d732d093fc3f120aca1b31a6bb484492f31081c1814a858e25c72e" default)))
+ '(doom-modeline-mode t)
  '(fci-rule-color "#505050")
  '(flycheck-color-mode-line-face-to-color (quote mode-line-buffer-id))
  '(frame-background-mode (quote dark))
@@ -92,12 +93,14 @@
  '(objed-cursor-color "#d02b61")
  '(package-selected-packages
    (quote
-    (command-log-mode markdown-mode+ pdf-tools smartparens ibuffer-vc doom-themes color-theme-solarized color-theme-sanityinc-solarized dracula-theme flycheck-rust cargo racer rust-mode ob-http minions org-noter jenkins org-attach-screenshot add-node-modules-path emmet-mode google-translate pg emacsql-psql eglot lsp-ui lsp-mode lsp-javascript-typescript parinfer 4clojure use-package tide solarized-theme real-auto-save rainbow-delimiters projectile prettier-js paren-face paredit org-plus-contrib org-bullets oceanic-theme ob-mongo ob-async ivy indium git-gutter-fringe exec-path-from-shell evil-surround evil-mc evil-magit evil-leader evil-easymotion evil-commentary elisp-format editorconfig diff-hl color-theme-sanityinc-tomorrow color-theme cider autopair auto-indent-mode ag ace-jump-mode)))
+    (pomidor alert company-lsp counsel-dash dash-docs auto-complete json-mode idle-highlight-mode clj-refactor command-log-mode markdown-mode+ pdf-tools smartparens ibuffer-vc doom-themes color-theme-solarized color-theme-sanityinc-solarized dracula-theme flycheck-rust cargo racer rust-mode ob-http minions org-noter jenkins org-attach-screenshot add-node-modules-path emmet-mode google-translate pg emacsql-psql eglot lsp-ui lsp-mode lsp-javascript-typescript parinfer 4clojure use-package tide solarized-theme real-auto-save rainbow-delimiters projectile prettier-js paren-face paredit org-plus-contrib org-bullets oceanic-theme ob-mongo ob-async ivy indium git-gutter-fringe exec-path-from-shell evil-surround evil-mc evil-magit evil-leader evil-easymotion evil-commentary elisp-format editorconfig diff-hl color-theme-sanityinc-tomorrow color-theme cider autopair auto-indent-mode ag ace-jump-mode)))
  '(pos-tip-background-color "#073642")
  '(pos-tip-foreground-color "#93a1a1")
+ '(show-paren-mode t)
  '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#073642" 0.2))
  '(term-default-bg-color "#002b36")
  '(term-default-fg-color "#839496")
+ '(tool-bar-mode nil)
  '(vc-annotate-background "#1b1d1e")
  '(vc-annotate-background-mode nil)
  '(vc-annotate-color-map
@@ -133,7 +136,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(mode-line ((t (:background "blue4" :foreground "gray90"))))
+ '(avy-background-face ((((class color) (min-colors 89)) (:foreground "#373b41"))) t)
+ '(font-lock-function-name-face ((t (:foreground "#7CA1EB"))))
+ '(font-lock-type-face ((t (:foreground "#F2C16F"))))
  '(mode-line-buffer-id ((t (:foreground "gold1" :weight ultra-bold))))
  '(mode-line-inactive ((t (:background "#404045" :foreground "gray60"))))
  '(org-block ((t (:background "#000000"))))
@@ -142,5 +147,8 @@
  '(org-block-end-line ((t (:foreground "#008ED1" :background "#002E41"))))
  '(show-paren-match ((t (:background "default" :foreground "#afa" :weight ultra-bold))))
  '(show-paren-mismatch ((t (:background "default" :foreground "#cc6666" :weight ultra-bold))))
+ '(twittering-username-face ((((class color) (min-colors 89)) (:inherit erc-pal-face))) t)
+ '(typescript-access-modifier-face ((t (:inherit font-lock-keyword-face :foreground "#BB8DD2"))))
+ '(typescript-this-face ((t (:inherit font-lock-keyword-face :foreground "#FA5571"))))
  '(vertical-border ((t (:foreground "#1d1f21"))))
  '(which-func ((t (:foreground "orange")))))
